d4881779a789a67256032246868587535807dbf8
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-07 21:51:01 +0530

    Modified Kart component

--- client/src/components/Options/Kart.jsx
+++ client/src/components/Options/Kart.jsx
@@ -4,8 +4,9 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 export default function Kart() {
 
     return (
-        <div className="kart">
-            <ShoppingCartIcon className="cart-icon" style={{fontSize:'2.1rem'}}/>
+        <div className="kart-cont">
+            <ShoppingCartIcon className="cart-icon" style={{fontSize:'2.6rem'}}/>
+            <div className="cart">0</div>
         </div>
-    )
+    ) 
 }
