b38cebff1b787d88b02df1b74db488d83c4629cf
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-12 21:06:12 +0530

    functionality to display popular tests and packs on home page

--- client/src/components/Popular-packs/Popular-pack-cont.jsx
+++ client/src/components/Popular-packs/Popular-pack-cont.jsx
@@ -1,20 +1,45 @@
 import Popular from "./Popular.jsx"
 import './PopularCont.css';
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
+import { Link } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
 
 export default function PopularPackCont() {
+    const [popularPacks, setPopularPacks] = useState([]);
+
+    useEffect(() => {
+        const fetchPopularPacks = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/home/popular-tests-packs");
+                const data = await res.json();
+                setPopularPacks(data.packages);
+            }
+            catch {
+                console.error("Error fetching popular packages:", err);
+            }
+        }
+        fetchPopularPacks();
+    }, [])
 
     return (
         <div className="popular-cont" style={{ backgroundColor: "pink" }}>
             <h2>Popular Packages</h2>
-            <div className="popular-items">
-                <Popular name="Specific Gravity Test" price={150}  />
-                <Popular name="Specific Gravity Test" price={150} />
-                <Popular name="Specific Gravity Test" price={150} />
-                <div className="more">
-                    <div className="more-text">See more </div>
-                    <ReadMoreIcon className="more-icon" style={{ fontSize: '3rem' }} />
-                </div>
+            <div className="popular-items popular-pack">
+                {popularPacks.map((pack) => (
+                    <Popular
+                        key={pack._id}
+                        name={pack.name}
+                        price={pack.price}
+                        tests={pack.tests} />
+                ))}
+
+                <Link to="/all-packages" className="link">
+                    <div className="more">
+                        <div className="more-text">See more </div>
+                        <ReadMoreIcon className="more-icon" style={{ fontSize: '3rem' }} />
+                    </div>
+                </Link>
             </div>
         </div>
     )
--- client/src/components/Popular-tests/Popular-test-cont.jsx
+++ client/src/components/Popular-tests/Popular-test-cont.jsx
@@ -1,20 +1,50 @@
 import Popular from "../Popular-packs/Popular.jsx";
 import '../Popular-packs/PopularCont.css';
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function PopularTestCont() {
+
+    const [popularTests, setPopularTests] = useState([]);
+
+    useEffect(() => {
+
+        const fetchPopularTests = async () => {
+
+            try {
+                const res = await fetch("http://localhost:5000/api/home/popular-tests-packs");
+                const data = await res.json();
+                setPopularTests(data.tests);
+            }
+
+            catch (err) {
+                console.error("Error fetching popular tests:", err);
+            }
+
+        }
+        fetchPopularTests();
+    }, [])
 
     return (
         <div className="popular-cont" style={{ backgroundColor: "pink" }}>
             <h2>Popular Tests</h2>
-            <div className="popular-items">
-                <Popular name="Specific Gravity Test" description={"Measures concentration of urine."} price={150} />
-                <Popular name="Specific Gravity Test" description={"Measures concentration of urine."} price={150} />
-                <Popular name="Specific Gravity Test" description={"Measures concentration of urine."} price={150} />
-                <div className="more">
-                    <div className="more-text">See more </div>
-                    <ReadMoreIcon className="more-icon" style={{ fontSize: '3rem' }} />
-                </div>
+            <div className="popular-items popular-test">
+
+                {popularTests.map((test) => (
+                    <Popular
+                        key={test._id}
+                        name={test.name}
+                        price={test.price}
+                    />
+                ))}
+
+                <Link to="/all-tests" className="link">
+                    <div className="more">
+                        <div className="more-text">See more </div>
+                        <ReadMoreIcon className="more-icon" style={{ fontSize: '3rem' }} />
+                    </div>
+                </Link>
             </div>
         </div>
     )
