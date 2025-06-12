9abc4bc48db937adfdb5505e88ca0c4b797b67b5
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-12 14:34:45 +0530

    Added All-packs component

---
+++ client/src/components/All-packs/All-packs-cont.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import Popular from "../Popular-packs/Popular.jsx";
+import '../Popular-packs/PopularCont.css';
+import '../Search-results/SearchResults.css';
+import './All-packs.css';
+import RelevanceCont from "../Relevance/Relevance-cont.jsx";
+
+
+export default function AllPacksCont() {
+    const [tests, setTests] = useState([]);
+
+    useEffect(() => {
+        const fetchAllTests = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/all-packages");
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                const data = await res.json();
+                setTests(data);
+            } catch (error) {
+                console.error("Error fetching all tests:", error);
+            }
+        };
+
+        fetchAllTests();
+    }, []);
+
+    return (
+        <div className="all-tests all-packs">
+            <div className="relevance">
+                <RelevanceCont />
+            </div>
+
+            <div className="search-tests-cont">
+                {tests.length > 0 ? (
+                    <>
+                        <div className="search-heading">
+                            <h1>All Package Services</h1>
+                        </div>
+                        <div className="search-tests">
+                            {tests.map((test) => (
+                                <Popular
+                                    key={test._id || test.name}
+                                    name={test.name}
+                                    price={test.price}
+                                    tests={test.tests}
+                                />
+                            ))}
+                        </div>
+                    </>
+                ) : (
+                    <p>Loading or no tests available.</p>
+
+                )}
+            </div>
+        </div>
+    );
+}
