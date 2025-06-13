28835f79f0495b48a7ab4133903b9394c7e9c593
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-13 20:04:50 +0530

    modified relevance field in data

--- server/data/data.js
+++ server/data/data.js
@@ -1,189 +1,34 @@
 const sampleTest = [
-
-  {
-    "name": "Fasting Blood Sugar",
-    "relevance": "Diabetes",
-    "price": 60,
-    "popular": true
-  },
-
-  {
-    "name": "Post Lunch Blood Sugar",
-    "relevance": "Diabetes",
-    "price": 60
-  },
-
-  {
-    "name": "Random Blood Sugar",
-    "relevance": "Diabetes",
-    "price": 60
-  },
-
-  {
-    "name": "Complete Urine Examination",
-    "relevance": "Urine",
-    "price": 100
-  },
-
-  {
-    "name": "Urine Culture Sensitivity",
-    "relevance": "Urine",
-    "price": 450
-  },
-
-  {
-    "name": "Erythrocyte Sedimentation Rate",
-    "relevance": "Blood",
-    "price": 110
-  },
-
-  {
-    "name": "Antistreptolysin O",
-    "relevance": "Blood",
-    "price": 300
-  },
-
-  {
-    "name": "Complete Blood Count",
-    "relevance": "Blood",
-    "price": 400,
-    "popular": true
-  },
-
-  {
-    "name": "C-Reactive Protein",
-    "relevance": "Blood",
-    "price": 450
-  },
-
-  {
-    "name": "Typhoid Test",
-    "relevance": "Blood",
-    "price": 300
-  },
-
-  {
-    "name": "Blood Group",
-    "relevance": "Blood",
-    "price": 100
-  },
-
-  {
-    "name": "Blood Urea",
-    "relevance": "Kidney",
-    "price": 200
-  },
-
-  {
-    "name": "Serum Creatinine",
-    "relevance": "Kidney",
-    "price": 200,
-    "popular": true
-  },
-
-  {
-    "name": "Electrolytes",
-    "relevance": "Kidney",
-    "price": 500
-  },
-
-  {
-    "name": "SR Uric Acid ",
-    "relevance": "Kidney",
-    "price": 300
-  },
-
-  {
-    "name": "SR Calcium",
-    "relevance": "Blood",
-    "price": 250
-  },
-
-  {
-    "name": "Liver Function Tests (LFT)",
-    "relevance": "Liver",
-    "price": 550
-  },
-
-  {
-    "name": "Lactate Dehydrogenase",
-    "relevance": "Liver",
-    "price": 500
-  },
-
-  {
-    "name": "Lipid Profile",
-    "relevance": "Lipid and Iron",
-    "price": 500,
-    "popular": true
-  },
-
-  {
-    "name": "Ferritin ",
-    "relevance": "Lipid and Iron",
-    "price": 800
-  },
-
-  {
-    "name": "TSH (Thyroid Stimulating Hormone)",
-    "relevance": "Thyroid",
-    "price": 300,
-    "popular": true
-  },
-
-  {
-    "name": "Thyroid Profile",
-    "relevance": "Thyroid",
-    "price": 550
-  },
-
-  {
-    "name": "HIV",
-    "relevance": "Infection",
-    "price": 500
-  },
-
-  {
-    "name": "Hepatitis B",
-    "relevance": "Infection",
-    "price": 450
-  },
-
-  {
-    "name": "Syphilis",
-    "relevance": "Infection",
-    "price": 450
-  },
-
-  {
-    "name": "Hepatitis C",
-    "relevance": "Infection",
-    "price": 550
-  },
-
-  {
-    "name": "HB Electrophoresis",
-    "relevance": "Genetic",
-    "price": 1200
-  },
-
-  {
-    "name": "D-Dimer ",
-    "relevance": "Genetic",
-    "price": 1500
-  },
-
-  {
-    "name": "Targeted Imaging for Fetal Anomalies",
-    "relevance": "Pregnancy",
-    "price": 1500
-  },
-
-  {
-    "name": "Ultrasound Abdomen",
-    "relevance": "Scan",
-    "price": 1100
-  }
+  { name: "Fasting Blood Sugar", relevance: "Diabetes", price: 60, popular: true },
+  { name: "Post Lunch Blood Sugar", relevance: "Diabetes", price: 60 },
+  { name: "Random Blood Sugar", relevance: "Diabetes", price: 60 },
+  { name: "Erythrocyte Sedimentation Rate", relevance: "Blood", price: 110 },
+  { name: "Antistreptolysin O", relevance: "Blood", price: 300 },
+  { name: "Complete Blood Count", relevance: "Blood", price: 400, popular: true },
+  { name: "C-Reactive Protein", relevance: "Blood", price: 450 },
+  { name: "Typhoid Test", relevance: "Blood", price: 300 },
+  { name: "Blood Group", relevance: "Blood", price: 100 },
+  { name: "Blood Urea", relevance: "Kidney", price: 200 },
+  { name: "Serum Creatinine", relevance: "Kidney", price: 200, popular: true },
+  { name: "Electrolytes", relevance: "Kidney", price: 500 },
+  { name: "SR Uric Acid ", relevance: "Kidney", price: 300 },
+  { name: "SR Calcium", relevance: "Blood", price: 250 },
+  { name: "Liver Function Tests (LFT)", relevance: "Liver", price: 550 },
+  { name: "Lactate Dehydrogenase", relevance: "Liver", price: 500 },
+  { name: "Lipid Profile", relevance: "Blood", price: 500, popular: true },
+  { name: "TSH (Thyroid Stimulating Hormone)", relevance: "Thyroid", price: 300, popular: true },
+  { name: "Thyroid Profile", relevance: "Thyroid", price: 550 },
+  { name: "HIV", relevance: "Infection", price: 500 },
+  { name: "Hepatitis B", relevance: "Infection", price: 450 },
+  { name: "Syphilis", relevance: "Infection", price: 450 },
+  { name: "Hepatitis C", relevance: "Infection", price: 550 },
+  { name: "Complete Urine Examination", relevance: "Kidney", price: 100 },
+  { name: "HB Electrophoresis", relevance: "Blood", price: 1200 },
+  { name: "D-Dimer ", relevance: "Blood", price: 1500 },
+  { name: "Ultrasound Abdomen", relevance: "Stomach", price: 1100 },
+  { name: "Ferritin ", relevance: "Blood", price: 800 },
+  { name: "Lactate Dehydrogenase", relevance: "Blood", price: 500 },
+  { name: "Vitamin D3", relevance: "Blood", price: 600 }
 ];
 
 const samplePack = [
@@ -433,6 +278,7 @@ const samplePack = [
     ]
   }
 ];
+
 
 
 export { sampleTest, samplePack };
