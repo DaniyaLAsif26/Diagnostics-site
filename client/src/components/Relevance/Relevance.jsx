87cd623ad854a7cb9f97c45bc6de1f7fb77be3fd
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-13 09:48:01 +0530

    Added hover effects

--- client/src/components/Relevance/Relevance.jsx
+++ client/src/components/Relevance/Relevance.jsx
@@ -8,7 +8,7 @@ export default function Relevance({relevance}) {
                 relevance.map((link) => (
                     <div className={link.name} style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', cursor: 'pointer',gap:'10px'
                     }}>
-                        <img src={link.img} alt="" />
+                        <img src={link.img} alt="" className='relevance-img' />
                         <div style={{fontSize:'1.26rem' , fontWeight:500}}>{link.name} </div>
                     </div>
                 ))
