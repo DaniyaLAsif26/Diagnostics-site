b57f70649e0d3443eabbe0be9222a049bd626ba3
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-14 11:13:13 +0530

    added Home relevance page and route logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import SliderCont from '../components/Slider/Slider-cont.jsx';
 import PopularPackCont from '../components/Popular-packs/Popular-pack-cont.jsx';
 import PopularTestCont from '../components/Popular-tests/Popular-test-cont.jsx';
@@ -5,11 +6,18 @@ import RelevanceCont from '../components/Relevance/Relevance-cont.jsx';
 import ChooseUsCont from '../components/Choose-us/Choose-us-cont.jsx';
 
 export default function Home() {
+
+  const navigate = useNavigate();
+
+  const handleRelevanceClick = (relevance) => {
+    navigate(`/relevance/${relevance}`);
+  };
+
   return (
     <>
       <SliderCont />
       <PopularPackCont />
-      <RelevanceCont />
+      <RelevanceCont onRelevanceClick={handleRelevanceClick} />
       <PopularTestCont />
       <ChooseUsCont />
     </>
---
+++ client/src/pages/HomeRelevancePage.jsx
@@ -0,0 +1,9 @@
+import HomeRelevance from "../components/Home-Relevance/Home-relevance";
+
+export default function HomeRelevancePage() {
+    return (
+        <>
+            <HomeRelevance />
+        </>
+    );
+}
