3556b047923eaf9fd3522ae6de08e5d1314cb8aa
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-15 10:43:23 +0530

    Added btn add remove functionality

--- client/src/components/Popular-packs/Popular.jsx
+++ client/src/components/Popular-packs/Popular.jsx
@@ -1,8 +1,25 @@
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import './Popular.css';
 
 export default function Popular({ name, price, tests }) {
+
+    const [message, setMessage] = useState('');
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        if (!clicked) {
+            setClicked(true);
+            setMessage('Added to cart');
+            setTimeout(() => setMessage(""), 3500);
+        } else {
+            setClicked(false);
+            setMessage('Removed from cart');
+            setTimeout(() => setMessage(""), 3500);
+        }
+    }
+
 
     return (
         <div className="popular">
@@ -14,10 +31,15 @@ export default function Popular({ name, price, tests }) {
             )}
             <div className="price-book">
                 <div className="">&#8377;{price}</div>
-                <Button variant="contained" endIcon={<AddShoppingCartIcon />}>
-                    Add
+                <Button
+                    variant="contained"
+                    onClick={handleClick}
+                    endIcon={<AddShoppingCartIcon />}>
+                    {clicked ? "Remove" : "Add"}
                 </Button>
             </div>
+            {/* {message && <div className="added-msg">{message}</div>}
+        */}
         </div>
     )
 }
