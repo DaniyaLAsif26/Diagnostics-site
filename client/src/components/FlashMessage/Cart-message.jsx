17cda000d2cff551e1decdf4190066f76912ec30
Daniyal Asif <daniyalasif550@gmail.com>
2025-06-24 17:50:07 +0530

    Styled the flash mssg

--- client/src/components/FlashMessage/Cart-message.jsx
+++ client/src/components/FlashMessage/Cart-message.jsx
@@ -6,15 +6,20 @@ export default function CartMessage() {
 
     useEffect(() => {
         if (message) {
-            const timer = setTimeout(() => clearMessage(), 3500);
+            const timer = setTimeout(() => clearMessage(), 3000);
             return () => clearTimeout(timer);
         }
     }, [message, clearMessage]);
 
     if (!message) return null;
     return (
-        <div className="global-flash">
-            {message}
+        <div className="flash-cont">
+            <div className="global-flash">
+                <div className="flash-msg">
+                    {message}
+                </div>
+                <div className="flash-border"></div>
+            </div>
         </div>
     );
 }
--- client/src/components/Options/Kart.jsx
+++ client/src/components/Options/Kart.jsx
@@ -1,6 +1,7 @@
 import './Kart.css';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useCart } from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
 export default function Kart() {
 
@@ -8,8 +9,10 @@ export default function Kart() {
 
     return (
         <div className="kart-cont">
-            <ShoppingCartIcon className="cart-icon" style={{ fontSize: '2.6rem' }} />
-            <div className="cart">{cartItem.length}</div>
+            <Link to="/cart-items" className="cart-link" >
+                <ShoppingCartIcon className="cart-icon" style={{ fontSize: '2.6rem' }} />
+                <div className="cart">{cartItem.length}</div>
+            </Link>
         </div>
     )
 }
