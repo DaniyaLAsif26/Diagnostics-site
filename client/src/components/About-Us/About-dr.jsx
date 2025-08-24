c30b6cd8ecf81930b830b226e0376dc7c59cb786
Daniyal Asif <daniyalasif550@gmail.com>
2025-08-24 23:16:13 +0530

    some changes in about-dr component

--- client/src/components/About-Us/About-dr.jsx
+++ client/src/components/About-Us/About-dr.jsx
@@ -12,7 +12,7 @@ export default function AboutDr() {
                     <div className="dr-degre">MBBS, MD(osm) Pathology</div>
                 </div>
                 <div className="dr-desc">
-                    Dr. Mohammad Irshad Ahmed, the son of the late Dr. Saleem Ahmed, now leads the center with a deep sense of purpose and responsibility. A highly qualified and compassionate medical professional, Dr. Irshad brings both modern medical knowledge and a strong ethical foundation to the center.
+                    Dr. Mohammad Irshad Ahmed, the son of the late Dr. Mohammed Rasheed Ahmed, now leads the center with a deep sense of purpose and responsibility. A highly qualified and compassionate medical professional, Dr. Irshad brings both modern medical knowledge and a strong ethical foundation to the center.
 
                     Under his leadership, the center has embraced the latest diagnostic technologies, streamlined patient care, and expanded its services—while staying true to the compassionate, patient-focused care his father envisioned. His mission is to make accurate and affordable diagnostics accessible to all, while ensuring a warm and trustworthy healthcare experience for every visitor.
                 </div>
