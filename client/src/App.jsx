47d680c8deec046831ee55b7c3696a8d8e67ab6d
Daniyal Asif <daniyalasif550@gmail.com>
2025-08-24 23:25:59 +0530

    Added login forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,9 @@ import OptionsBar from './components/options/Options-bar.jsx';
 import ScrollToTop from './components/Scroll-top/Scroll-to-top.jsx';
 import BackToTop from "./components/Back-to-top/BackToTop";
 import CartMessage from './components/FlashMessage/Cart-message.jsx';
+import LoginForm from './components/Login/Login.jsx';
+import OtpInput from './components/Login/Otp.jsx';
+import AdminLoginForm from './components/Login/adminLogin.jsx';
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
       <ScrollToTop />
       <Navbar />
       <OptionsBar />
+      <LoginForm />
+      <OtpInput />
+      <AdminLoginForm />
       <AppRoutes />
       <Footer />
       <Copyright />
