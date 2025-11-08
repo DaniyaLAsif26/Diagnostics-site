919f10c01d6e67957feee7466b03bbbc9d45cc89
Daniyal Asif <daniyalasif550@gmail.com>
2025-11-08 14:46:58 +0530

    production-ready server config

--- server/server.js
+++ server/server.js
@@ -13,15 +13,22 @@ import UserRoute from './routes/User.js';
 import ReportRoute from './routes/UploadReports.js'
 
 import cookieParser from "cookie-parser";
+app.use(cookieParser());
 
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
-const PORT = 5000;
-app.use(cookieParser());
+import compression from 'compression';
+import helmet from 'helmet';
+app.use(helmet());
+app.use(compression());
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", "https://your-vercel-url.vercel.app", "https://visiondiagnosticscentre.com/"];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -35,10 +42,7 @@ app.use(cors({
   credentials: true
 }));
 
-
 // app.use(cors())
-
-app.use(express.json());
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/vision-center";
 const MONGO_URL = process.env.ATLAS_DB_URL;
@@ -55,10 +59,6 @@ app.use('/api/save', saveAppointmentRoute);
 app.use('/api/search', searchRoutes);
 app.use('/api/home', homePopularRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.use("/api", (req, res) => {
   res.status(404).json({ message: "API route not found" });
 });
@@ -73,4 +73,8 @@ app.use((err, req, res, next) => {
     success: false,
     message: err.message || "Internal Server Error",
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
