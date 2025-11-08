3a6d8861dbf50308bc41109c87ad6f26b1dcdd06
Daniyal Asif <daniyalasif550@gmail.com>
2025-11-08 19:22:51 +0530

    edited clod config file

--- server/cloudConfig.js
+++ server/cloudConfig.js
@@ -1,22 +1,20 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
-    api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: 'diagnostic_reports',
-        allowedFormats: ['pdf'],
-        resource_type: 'raw',
-        public_id: (req, file) => {
-            return file.originalname;
-        }
-    },
+  cloudinary,
+  params: {
+    folder: 'diagnostic_reports',
+    allowedFormats: ['pdf'],
+    resource_type: 'raw',
+    public_id: (req, file) => file.originalname,
+  },
 });
 
-module.exports = { cloudinary, storage };
+export { cloudinary, storage };
