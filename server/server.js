d7869e4e25ceef6fbc7ccfbb504c6e0495cb4bde
Daniyal Asif <daniyalasif550@gmail.com>
2025-11-09 02:37:42 +0530

    Edited vercel origin

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const HOST = '0.0.0.0';
 console.log('🔍 Environment PORT:', process.env.PORT);
 console.log('🔍 Using PORT:', PORT);
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://diagnostics-site-frontend-f22gtkka0.vercel.app/", "https://visiondiagnosticscentre.com"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://diagnostics-site-frontend.vercel.app", "https://visiondiagnosticscentre.com", "https://visiondiagnosticscentre.com"];
 
 app.use(cors({
   origin: function (origin, callback) {
