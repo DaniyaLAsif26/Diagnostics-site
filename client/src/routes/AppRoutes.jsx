7ed7ff1bd8202c451ea44f60f3b6dad5a70ebf6d
Daniyal Asif <daniyalasif550@gmail.com>
2025-11-09 16:10:52 +0530

    resolved not found

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home.jsx';
 import Search from '../pages/Search.jsx';
-import NotFoundPage from '../pages/NotFound.jsx';
+import NotFoundPage from '../pages/NotFoundPage.jsx';
 import LaboratoryTestsPage from '../pages/LaboratoryTestsPage.jsx'
 import RadiologyTestsPage from '../pages/RadiologyTestsPage.jsx';
 import AboutUs from '../pages/AboutUs.jsx';
