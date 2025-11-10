10700183bfdf5d50591342f5b0b099b1747dfc5b
Daniyal Asif <daniyalasif550@gmail.com>
2025-11-10 18:10:51 +0530

    edited logout routes

--- server/routes/logOut.js
+++ server/routes/logOut.js
@@ -4,20 +4,18 @@ const router = express.Router();
 router.post("/user", (req, res) => {
     res.clearCookie("usertoken", {
         httpOnly: true,
-        // secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-    })
-
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    });
     return res.json({ Success: true })
 })
 
 router.post('/admin', (req, res) => {
     res.clearCookie("adminToken", {
         httpOnly: true,
-        // secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-    })
-
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    });
     res.json({ Success: true })
 })
 
