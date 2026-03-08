e2952432afe7aab4731f2ee61273397a9a222861
Daniyal Asif <daniyalasif550@gmail.com>
2026-03-09 03:11:59 +0530

    Chnaged packages schema

--- server/models/package.js
+++ server/models/package.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 const packSchema = new Schema({
   name: { type: String, required: true },
-  relevance: { type: [String], default: [] },
+  relevance: Array,
   price: { type: Number, required: true },
+  discountPrice: { type: Number, default: 0 },
   popular: { type: Boolean, default: false },
+  tests: Array 
 }, { timestamps: true });
 
 const Package = mongoose.model("Package", packSchema);
