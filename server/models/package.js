9b1bb58b13b8a52e46b8799455d25d3d518582cf
Daniyal Asif <daniyalasif550@gmail.com>
2026-03-09 02:49:14 +0530

    changed tests and packs schema

--- server/models/package.js
+++ server/models/package.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const packSchema = new Schema({
-  name: String,
-  relevance: Array,
-  price: Number,
-  popular: Boolean,
-  tests: Array  
-});
+  name: { type: String, required: true },
+  relevance: { type: [String], default: [] },
+  price: { type: Number, required: true },
+  popular: { type: Boolean, default: false },
+}, { timestamps: true });
 
 const Package = mongoose.model("Package", packSchema);
 
-export default Package ;
+export default Package;
--- server/models/test.js
+++ server/models/test.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const testSchema = new Schema({
-  name: String,
-  category: String,
+  name: { type: String, required: true },
+  category: { type: String, required: true },
   relevance: String,
   patientPreparation: String,
-  price: Number,
-  popular: Boolean,
-});
+  price: { type: Number, required: true },
+  discountPrice: { type: Number, default: 0 },
+  popular: { type: Boolean, default: false },
+}, { timestamps: true });
 
 const Test = mongoose.model("Test", testSchema);
 
